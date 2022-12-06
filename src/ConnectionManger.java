d501f73bab85a42298b012fe715498c42e58dd4d
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2022-12-06 02:57:13 -0500

    Created ConnectionManger class

---
+++ src/ConnectionManger.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionManger {
+
+    private Connection connection;
+    private Statement statement;
+
+    /* JDBC driver name and database URL */
+    private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/COMP3005Project";
+
+    /* Database credentials */
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public ConnectionManger() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            statement = connection.createStatement();
+        }
+        catch(SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public boolean executeQuery(String query) {
+        try {
+            statement.execute(query);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/StoreModel.java
+++ src/StoreModel.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.sql.*;
 
 public class StoreModel {
 
@@ -8,20 +7,14 @@ public class StoreModel {
     private final ArrayList<Tracker> trackers;
     private final HashMap<Book, Integer> inventory;
     private final ArrayList<StoreView> views;
-
-    /* JDBC driver name and database URL */
-    private static final String JDBC_DRIVER = "org.postgresql.Driver";
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/COMP3005Project";
-
-    /* Database credentials */
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "1234";
+    private final ConnectionManger connectionManger;
 
     public StoreModel() {
         users = new ArrayList<>();
         trackers = new ArrayList<>();
         inventory = new HashMap<>();
         views = new ArrayList<>();
+        connectionManger = new ConnectionManger();
     }
 
     public ArrayList<User> getUsers() {
@@ -34,29 +27,5 @@ public class StoreModel {
 
     public HashMap<Book, Integer> getInventory() {
         return inventory;
-    }
-
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-
-        try {
-            Class.forName(JDBC_DRIVER);
-            System.out.println("Connection to a database...");
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-
-            System.out.println("Executing SQL query...");
-            statement = connection.createStatement();
-
-            String sql = "";
-            statement.execute(sql);
-            System.out.println("Query executed successfully!");
-            statement.close();
-            connection.close();
-        }
-        catch(SQLException | ClassNotFoundException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
     }
 }

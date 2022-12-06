ced42d9f57d3c844e3d07ce3b09f5603cf027d5a
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2022-12-06 01:56:45 -0500

    Model to SQL connection testing and View updates

--- src/StoreModel.java
+++ src/StoreModel.java
@@ -1,9 +1,62 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.*;
 
 public class StoreModel {
 
     private final ArrayList<User> users;
     private final ArrayList<Tracker> trackers;
     private final HashMap<Book, Integer> inventory;
+    private final ArrayList<StoreView> views;
+
+    /* JDBC driver name and database URL */
+    private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/COMP3005Project";
+
+    /* Database credentials */
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1234";
+
+    public StoreModel() {
+        users = new ArrayList<>();
+        trackers = new ArrayList<>();
+        inventory = new HashMap<>();
+        views = new ArrayList<>();
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public ArrayList<Tracker> getTrackers() {
+        return trackers;
+    }
+
+    public HashMap<Book, Integer> getInventory() {
+        return inventory;
+    }
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connection to a database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+            System.out.println("Executing SQL query...");
+            statement = connection.createStatement();
+
+            String sql = "";
+            statement.execute(sql);
+            System.out.println("Query executed successfully!");
+            statement.close();
+            connection.close();
+        }
+        catch(SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
 }
--- src/StoreView.java
+++ src/StoreView.java
@@ -1,2 +1,15 @@
 public interface StoreView {
+
+    /**
+     * Handles a message.
+     * @param message Message to handle.
+     */
+    void handleMessage(String message);
+
+    /**
+     * Gets a user's input.
+     * @param message Message for input request.
+     * @return A user's input.
+     */
+    String getUserInput(String message);
 }
